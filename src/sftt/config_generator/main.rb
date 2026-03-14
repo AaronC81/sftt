d389b6b6ee3192e526d99da9ca646bc970510981
Aaron Christiansen <aaronc20000@gmail.com>
2026-03-14 15:40:54 +0000

    Add `--input`/`--output` command-line arguments for OTP config generator

--- src/sftt/config_generator/main.rb
+++ src/sftt/config_generator/main.rb
@@ -1,22 +1,37 @@
 # TODO: This should also take in tube data, somehow
-# TODO: add command-line options for directories
 
 require 'fileutils'
+require 'optparse'
 
 require_relative 'dtd_to_gtfs'
 require_relative 'foot_interchange_generator'
 
-INPUT_DIRECTORY = File.join(__dir__, '..', 'inputs')
+input_dir = nil
+output_dir = nil
 
-OTP_DIRECTORY = File.join(__dir__, '..', 'scratch', 'otp')
-FileUtils.mkdir_p(OTP_DIRECTORY)
+OptionParser.new do |parser|
+  parser.on("--input DIR", "Directory with input files") do |dir|
+    raise '--input specified more than once' if input_dir
+    input_dir = dir
+  end
 
-INPUT_TIMETABLE_ZIP = File.join(INPUT_DIRECTORY, 'timetable.zip')
+  parser.on("--output DIR", "Directory for generated OpenTripPlanner data files") do |dir|
+    raise '--output specified more than once' if output_dir
+    output_dir = dir
+  end
+end.parse!
+
+raise '--input is required' unless input_dir
+raise '--output is required' unless output_dir
+
+FileUtils.mkdir_p(output_dir)
+
+INPUT_TIMETABLE_ZIP = File.join(input_dir, 'timetable.zip')
 unless File.exist?(INPUT_TIMETABLE_ZIP)
   abort "Input file missing: #{INPUT_TIMETABLE_ZIP}"
 end
 
-OUTPUT_TIMETABLE_GTFS_DIR = File.join(OTP_DIRECTORY, 'timetable_gtfs')
+OUTPUT_TIMETABLE_GTFS_DIR = File.join(output_dir, 'timetable_gtfs')
 
 dtd_to_gtfs = Sftt::ConfigGenerator::DtdToGtfs.new(
   INPUT_TIMETABLE_ZIP,
