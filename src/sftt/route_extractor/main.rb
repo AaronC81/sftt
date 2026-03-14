9606ddca0551d57cb4cbb2b3c0a138647a77a848
Aaron Christiansen <aaronc20000@gmail.com>
2026-03-14 18:13:20 +0000

    Add command-line options for route extractor

--- src/sftt/route_extractor/main.rb
+++ src/sftt/route_extractor/main.rb
@@ -5,62 +5,106 @@
 require 'fileutils'
 require 'json'
 require 'concurrent'
+require 'optparse'
+require 'time'
+
 require_relative '../common/open_trip_planner'
 
-OTP_CONFIG_DIRECTORY = File.join(__dir__, '..', '..', '..', 'scratch', 'otp')
-OTP_JAR = File.join(__dir__, '..', '..', '..', 'inputs', 'otp-shaded-2.8.1.jar')
-otp_server = Sftt::OpenTripPlanner::Server.new(OTP_JAR, OTP_CONFIG_DIRECTORY)
+otp_config_dir = nil
+otp_jar = nil
+output_file = nil
+
+to_station = nil
+date = Time.now.iso8601
+search_window = 120
+
+OptionParser.new do |parser|
+  parser.on("--otp-config DIR", "Directory with OpenTripPlanner config") do |dir|
+    raise '--otp-config specified more than once' if otp_config_dir
+    otp_config_dir = dir
+  end
+
+  parser.on("--otp-jar FILE", "Path to OpenTripPlanner JAR file") do |jar|
+    raise '--otp-jar specified more than once' if otp_jar
+    otp_jar = jar
+  end
+
+  parser.on("--output FILE", "Path to route JSON file to generate") do |file|
+    raise '--output specified more than once' if output_file
+    output_file = file
+  end
+
+  parser.on("--to ID", "TIPLOC of destination station to calculate routes to") do |id|
+    raise '--to specified more than once' if to_station
+    to_station = id
+  end
+
+  parser.on("--date DATE", "ISO 8601 datetime for routing start time. Default now") do |d|
+    raise '--date specified more than once' if date
+    date = d
+  end
+
+  parser.on("--window WINDOW", "Number of minutes to search after the --date. Default #{search_window}") do |window|
+    raise '--window specified more than once' if search_window
+    search_window = Integer(window)
+  end
+end.parse!
+
+raise '--otp-config is required' unless otp_config_dir
+raise '--otp-jar is required' unless otp_jar
+raise '--output is required' unless output_file
+raise '--to is required' unless to_station
+raise '--date is required' unless date
+
+otp_server = Sftt::OpenTripPlanner::Server.new(otp_jar, otp_config_dir)
 otp_server.start(build_graph: true)
 at_exit do
   otp_server.stop
 end
 
-OUTPUT_DIRECTORY = File.join(__dir__, '..', '..', '..', 'scratch', 'routes')
-FileUtils.mkdir_p(OUTPUT_DIRECTORY)
-
 otp = Sftt::OpenTripPlanner::GraphQLConnection.new
 quays = otp.query_quays
+puts "Loaded #{quays.length} quays"
 
 def quay_to_tiploc(quay)
   quay.split(':').last
 end
 
-quays.each do |from_quay|
-  puts "#{from_quay}"
-  all_routes = {}
-
-  thread_pool = Concurrent::FixedThreadPool.new(16)
-  quays.each do |to_quay|
-    thread_pool.post do
-      puts "  -> #{to_quay}"
-
-      response = otp.query_trip(from_quay, to_quay)
-      
-      # TODO: prune to "best" route if there are multiple (e.g. LEEDS -> YORK, many direct trains)
-      routes = []
-      response.to_h['data']['trip']['tripPatterns'].each do |trip|
-        routes << {
-          legs: trip['legs']
-            .map do |leg| 
-              {
-                mode: leg['mode'],
-                from: quay_to_tiploc(leg['fromPlace']['quay']['id']),
-                to: quay_to_tiploc(leg['toPlace']['quay']['id']),
-                duration: leg['duration'],
-              }
-            end
-        }
-      end
-      
-      all_routes[quay_to_tiploc(to_quay)] = { routes: }
-    end
-  end
-
-  thread_pool.shutdown
-  thread_pool.wait_for_termination
-
-  File.write(
-    File.join(OUTPUT_DIRECTORY, "#{quay_to_tiploc(from_quay)}.json"),
-    all_routes.to_json,
-  )
+# Assuming that there's only one transit source so the prefix is `1:`
+to_quay = "1:#{to_station}"
+unless quays.include?(to_quay)
+  raise 'unknown --to station'
 end
+
+all_routes = {}
+thread_pool = Concurrent::FixedThreadPool.new(16)
+quays.each do |from_quay|
+  thread_pool.post do
+    puts "  #{from_quay}"
+
+    response = otp.query_trip(from_quay, to_quay)
+    
+    # TODO: prune to "best" route if there are multiple (e.g. LEEDS -> YORK, many direct trains)
+    routes = []
+    response.to_h['data']['trip']['tripPatterns'].each do |trip|
+      routes << {
+        legs: trip['legs']
+          .map do |leg| 
+            {
+              mode: leg['mode'],
+              from: quay_to_tiploc(leg['fromPlace']['quay']['id']),
+              to: quay_to_tiploc(leg['toPlace']['quay']['id']),
+              duration: leg['duration'],
+            }
+          end
+      }
+    end
+    
+    all_routes[quay_to_tiploc(from_quay)] = { routes: }
+  end
+end
+
+thread_pool.shutdown
+thread_pool.wait_for_termination
+
+File.write(output_file, all_routes.to_json)
