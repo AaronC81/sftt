5faa909f348a32fb1a2a24865c13f0e62959dfdc
Aaron Christiansen <aaronc20000@gmail.com>
2026-03-24 10:01:04 +0000

    Use Elizabeth line during TfL routing

--- data/tube_routes/fetch_tube_info.rb
+++ data/tube_routes/fetch_tube_info.rb
@@ -5,7 +5,7 @@ require_relative 'data'
 TFL_APP_KEY = ENV['SFTT_TFL_APP_KEY'] or abort 'missing API key'
 
 def fetch_tfl_routes(from_naptan, to_naptan, modes:, include_entrance_walking:)
-  result = `curl \"https://api.tfl.gov.uk/Journey/JourneyResults/#{from_naptan}/to/#{to_naptan}?app_key=#{TFL_APP_KEY}&mode=#{modes}&routeBetweenEntrances=#{include_entrance_walking ? 'true' : 'false'}&date=20260322&time=1030\"`
+  result = `curl \"https://api.tfl.gov.uk/Journey/JourneyResults/#{from_naptan}/to/#{to_naptan}?app_key=#{TFL_APP_KEY}&mode=#{modes}&routeBetweenEntrances=#{include_entrance_walking ? 'true' : 'false'}&date=20260323&time=1030\"`
   raise 'curl failed' unless $?.success?
   result
 end
@@ -19,13 +19,13 @@ CRS_TO_NAPTAN.each do |from_crs, from_naptan|
       Thread.new do
         File.write(
           File.join(__dir__, 'routes', "#{from_crs}_#{to_crs}_any_without_transfers.json"),
-          fetch_tfl_routes(from_naptan, to_naptan, modes: 'tube,walking', include_entrance_walking: false),
+          fetch_tfl_routes(from_naptan, to_naptan, modes: 'tube,elizabeth-line,walking', include_entrance_walking: false),
         )
       end,
       Thread.new do
         File.write(
           File.join(__dir__, 'routes', "#{from_crs}_#{to_crs}_any_with_transfers.json"),
-          fetch_tfl_routes(from_naptan, to_naptan, modes: 'tube,walking', include_entrance_walking: true),
+          fetch_tfl_routes(from_naptan, to_naptan, modes: 'tube,elizabeth-line,walking', include_entrance_walking: true),
         )
       end,
       Thread.new do
